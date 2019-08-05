256475aa46a3a8df961db89cee16d185f34dd064
Jamie Strange <jamie.strange@unosquare.com>
2019-08-05 10:59:27 +0100

    initial commit

---
+++ src/Details.js
@@ -0,0 +1,83 @@
+import React from "react";
+import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import ThemeContext from "./ThemeContext";
+
+class Details extends React.Component {
+  state = { loading: true, showModal: false };
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        url: animal.url,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      name,
+      showModal
+    } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`} </h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.adopt}>Yes</button>
+                <button onClick={this.toggleModal}>No, I'm a monster</button>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
