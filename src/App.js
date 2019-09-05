94990fc94531badb5a452c77fbbf0ed560035447
Jamie Strange <jamie.strange@unosquare.com>
2019-09-05 09:46:47 +0100

    initial commit for create character page and functionality

--- src/App.js
+++ src/App.js
@@ -5,24 +5,23 @@ import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+import { Toggle } from "./Toggle";
 
 const App = () => {
   const themeHook = useState("peru");
   return (
-    <React.StrictMode>
-      <ThemeContext.Provider value={themeHook}>
-        <div>
-          {/*           <header>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        {/*           <header>
             <Link to="/">Labyrinth Lords DnD Tool</Link>
           </header> */}
-          <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </div>
-      </ThemeContext.Provider>
-    </React.StrictMode>
+        <NavBar />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>{" "}
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
